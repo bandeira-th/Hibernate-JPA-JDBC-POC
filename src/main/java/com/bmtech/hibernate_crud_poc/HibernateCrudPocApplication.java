faf7461a2ae73e3141f0c62cf56c79cc40fd055b
bandeira-th <bandeira.trabalho@gmail.com>
2024-06-21 15:52:01 -0300

    inital commit - project-setup

---
+++ src/main/java/com/bmtech/hibernate_crud_poc/HibernateCrudPocApplication.java
@@ -0,0 +1,13 @@
+package com.bmtech.hibernate_crud_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateCrudPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateCrudPocApplication.class, args);
+	}
+
+}
