d76d7c7ab232a94ced2612a7419b2acc5b59cd3b
bandeira-th <bandeira.trabalho@gmail.com>
2024-06-21 16:14:10 -0300

    feat: CommandLineRunner Bean

--- src/main/java/com/bmtech/hibernate_crud_poc/HibernateCrudPocApplication.java
+++ src/main/java/com/bmtech/hibernate_crud_poc/HibernateCrudPocApplication.java
@@ -1,13 +1,23 @@
 package com.bmtech.hibernate_crud_poc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateCrudPocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateCrudPocApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
 	}
 
 }
