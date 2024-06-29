9e7ce7670f5429a4ede4e73a60624a5b0ecd4542
bandeira-th <bandeira.trabalho@gmail.com>
2024-06-29 00:27:36 -0300

    feat: StudentDAO implementation

--- src/main/java/com/bmtech/hibernate_crud_poc/dao/StudentDAOImpl.java
+++ src/main/java/com/bmtech/hibernate_crud_poc/dao/StudentDAOImpl.java
@@ -1,2 +1,26 @@
-package com.bmtech.hibernate_crud_poc.dao;public class StudentDAOImpl {
+package com.bmtech.hibernate_crud_poc.dao;
+
+import com.bmtech.hibernate_crud_poc.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    //define field to entity manager
+    EntityManager entityManager;
+
+    //inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional //from spring framework
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
 }
