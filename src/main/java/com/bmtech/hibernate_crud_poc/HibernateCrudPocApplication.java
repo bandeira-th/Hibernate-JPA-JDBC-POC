db88df95ee6c4791c4e1bd959aa7c9382dd52434
bandeira-th <trabalho.bandeira@gmail.com>
2024-07-08 19:21:50 -0300

    feat: added retrieve object func to CommandLineRunner app

--- src/main/java/com/bmtech/hibernate_crud_poc/HibernateCrudPocApplication.java
+++ src/main/java/com/bmtech/hibernate_crud_poc/HibernateCrudPocApplication.java
@@ -1,9 +1,12 @@
 package com.bmtech.hibernate_crud_poc;
 
+import com.bmtech.hibernate_crud_poc.dao.StudentDAO;
+
+import com.bmtech.hibernate_crud_poc.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -14,10 +17,26 @@ public class HibernateCrudPocApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDAO);
 		};
 	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating student...");
+		Student myStudent = new Student("Jon", "Smith", "js@mail.com");
+		Student myStudentB = new Student("Jane", "Smith", "js@mail.com");
+		System.out.println("Saving student...");
+		studentDAO.save(myStudent);
+		studentDAO.save(myStudentB);
+
+		System.out.println("Retrieving student...");
+		Student retrievedStudent = studentDAO.findById(myStudent.getId());
+		Student retrievedStudentB = studentDAO.findById(myStudentB.getId());
+
+		System.out.println("The students have been retrieved: " + retrievedStudent + " and " + retrievedStudentB);
+	}
+
 
 }
