b1938a69d2eac09ef29da923822adb9491bfef8f
bandeira-th <trabalho.bandeira@gmail.com>
2024-07-08 20:54:37 -0300

    feat: added findBylastName method to StudentDAO interface and implementation

--- src/main/java/com/bmtech/hibernate_crud_poc/dao/StudentDAOImpl.java
+++ src/main/java/com/bmtech/hibernate_crud_poc/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.bmtech.hibernate_crud_poc.dao;
 
 import com.bmtech.hibernate_crud_poc.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -27,5 +30,17 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll(){
+        TypedQuery<Student> students = entityManager.createQuery("FROM Student", Student.class);
+        return students.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> queryLastName = entityManager.createQuery("select s FROM Student s WHERE s.lastName = :lastName", Student.class);
+        return queryLastName.setParameter("lastName", lastName).getResultList();
     }
 }
