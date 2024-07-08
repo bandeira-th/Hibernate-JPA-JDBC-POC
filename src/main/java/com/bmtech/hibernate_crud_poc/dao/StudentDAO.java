b87b0b96c4df806fa1f661d1ba259696e6b6b081
bandeira-th <trabalho.bandeira@gmail.com>
2024-07-08 20:20:08 -0300

    feat: added findAll method to StudentDAO

--- src/main/java/com/bmtech/hibernate_crud_poc/dao/StudentDAO.java
+++ src/main/java/com/bmtech/hibernate_crud_poc/dao/StudentDAO.java
@@ -2,10 +2,14 @@ package com.bmtech.hibernate_crud_poc.dao;
 
 import com.bmtech.hibernate_crud_poc.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student student);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 
 }
